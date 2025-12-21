597b37bc2dc17b9e816e43c08ffd3b605ccaf0a5
abdopcnet <abdopcnet@gmail.com>
2025-12-21 21:24:45 +0000

    ❌ service_line.py

--- power_app/power_app/doctype/service_line/service_line.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Hadeel Milad and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class serviceline(Document):
-	pass
