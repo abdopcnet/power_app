b41928fcefd586183d3b7338a95dcf4bf3cdea92
abdopcnet <abdopcnet@gmail.com>
2025-12-21 21:24:45 +0000

    ❌ service_line.js

--- power_app/power_app/doctype/service_line/service_line.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, Hadeel Milad and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("service line", {
-// 	refresh(frm) {
-
-// 	},
-// });
