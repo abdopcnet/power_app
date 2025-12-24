23e403b03e03fa9fd3d044c0820f8b0f0c7c9ecc
abdopcnet <abdopcnet@gmail.com>
2025-12-24 00:19:56 +0000

    🔄 supplier_quotation.js

--- power_app/public/js/supplier_quotation.js
+++ power_app/public/js/supplier_quotation.js
@@ -19,7 +19,57 @@ frappe.ui.form.on('Supplier Quotation', {
 			update_total_expenses(frm);
 		}
 	},
+	custom_expense_template: function (frm) {
+		// Only fetch template data for draft/new documents
+		if (frm.doc.docstatus !== 0 && !frm.doc.__islocal) {
+			return;
+		}
+
+		if (frm.doc.custom_expense_template) {
+			fetch_expense_template(frm);
+		} else {
+			// Clear expenses if template is cleared
+			frm.clear_table('custom_service_expense');
+			frm.refresh_field('custom_service_expense');
+			update_total_expenses(frm);
+		}
+	},
 });
+
+/**
+ * Fetch expense template data and populate custom_service_expense child table
+ */
+function fetch_expense_template(frm) {
+	if (!frm.doc.custom_expense_template) {
+		return;
+	}
+
+	frappe.call({
+		method: 'power_app.supplier_quotation.get_expense_template_data',
+		args: {
+			template_name: frm.doc.custom_expense_template,
+		},
+		callback: function (r) {
+			if (!r.exc && r.message && r.message.length > 0) {
+				// Clear existing expenses
+				frm.clear_table('custom_service_expense');
+
+				// Add expenses from template
+				r.message.forEach(function (expense) {
+					let row = frm.add_child('custom_service_expense');
+					row.service_expense_type = expense.service_expense_type;
+					row.company = expense.company;
+					row.default_account = expense.default_account;
+					row.amount = expense.amount;
+					row.description = expense.description;
+				});
+
+				frm.refresh_field('custom_service_expense');
+				update_total_expenses(frm);
+			}
+		},
+	});
+}
 
 /**
  * Calculate and update total expenses in custom_total_expenses field
